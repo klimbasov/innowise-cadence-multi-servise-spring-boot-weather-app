9db6f46f8f2bc557be77ec2461c833190c17bc4e
compcoon <klimbasov.al@gmail.com>
2022-08-18 12:05:50 +0300

    Activity worker is now marked as non web application and does not take tcp port. Success response status of "launch" endpoint is 201 created.

--- activity-worker/src/main/java/com/innowise/weather/activityworker/Application.java
+++ activity-worker/src/main/java/com/innowise/weather/activityworker/Application.java
@@ -1,12 +1,13 @@
 package com.innowise.weather.activityworker;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class).web(WebApplicationType.NONE).run(args);
     }
 }
--- workflow-launcher/src/main/java/com/innowise/weather/launcher/controller/TaskController.java
+++ workflow-launcher/src/main/java/com/innowise/weather/launcher/controller/TaskController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("testLauncher")
@@ -20,6 +21,7 @@ public class TaskController {
     }
 
     @PostMapping("/launch")
+    @ResponseStatus(HttpStatus.CREATED)
     ResponseEntity<WeatherRecordDto> launch(@RequestParam String city){
         WeatherRecordDto dto = service.launch(city);
         return new ResponseEntity<>(dto, HttpStatus.OK);
